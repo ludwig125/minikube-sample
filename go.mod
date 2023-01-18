116e6615561f4e02b8fd09f494c98e10f66df822
ludwig125 <18366858+ludwig125@users.noreply.github.com>
2023-01-19 06:47:48 +0900

    pprof

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ludwig125/minikube-sample
+
+go 1.17
---
+++ statsd.go
@@ -0,0 +1,38 @@
+package main
+
+// https://artem.krylysov.com/blog/2017/03/13/profiling-and-optimizing-go-web-applications/#heap-profile
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type StatsD struct {
+	Namespace  string
+	SampleRate float64
+}
+
+func (s *StatsD) Send(stat string, kind string, delta float64) {
+	buf := fmt.Sprintf("%s.", s.Namespace)
+	trimmedStat := strings.NewReplacer(":", "_", "|", "_", "@", "_").Replace(stat)
+	buf += fmt.Sprintf("%s:%f|%s", trimmedStat, delta, kind)
+	if s.SampleRate != 0 && s.SampleRate < 1 {
+		buf += fmt.Sprintf("|@%s", strconv.FormatFloat(s.SampleRate, 'f', -1, 64))
+	}
+	ioutil.Discard.Write([]byte(buf)) // TODO: Write to a socket
+}
+
+func (s *StatsD) Incr(stat string) {
+	s.Send(stat, "c", 1)
+}
+
+func (s *StatsD) Histogram(stat string, value float64) {
+	s.Send(stat, "h", value)
+}
+
+func (s *StatsD) Timing(stat string, value time.Duration) {
+	s.Send(stat, "ms", value.Seconds()*1000)
+}
